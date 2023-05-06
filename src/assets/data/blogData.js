e8702d08bb990a37388ed5b0c3fda7eab863b584
Tuan Nghia <tuannghia20062003@gmmail.com>
2023-05-07 00:11:54 +0700

    Edit UI

--- src/assets/data/blogData.js
+++ src/assets/data/blogData.js
@@ -12,9 +12,9 @@ const blogData = [
     time: "9h",
     imgUrl: img01,
     description:
-      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+      " Lái xe luôn là một thử thách cho những người mới bắt đầu. Chi tiết cách lái xe số tự động sẽ giúp các bạn tự tin và an toàn trên mọi cung đường. Đối với các tài mới, khâu tìm hiểu cách lái xe số tự động trước khi cầm vô-lăng là rất cần thiết, nhằm tránh những rủi ro không đáng có. Trước khi sử dụng bất kỳ loại xe hơi nào, người lái xe cần nắm được các quy tắc cơ bản để bảo đảm an toàn khi thực hành lái xe. Việc tìm hiểu luật lái xe ở Việt Nam là điều mà bất kỳ ai có ý định học lái xe cần nắm rõ. Trong đó có một số nguyên tắc cơ bản mà người lái xe số tự động cần biết như: dừng xe cho người đi bộ, tuân thủ biển báo giao thông, ở trong giới hạn tốc độ, xử lý tình huống khẩn cấp và thắt dây an toàn.",
     quote:
-      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+      " Nhanh một phút, chậm cả đời!",
   },
 
   {
@@ -25,9 +25,9 @@ const blogData = [
     time: "13h",
     imgUrl: img02,
     description:
-      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+      " Mặc dù nhiều bộ phận rất cần thiết cho hoạt động của một chiếc ô tô, nhưng chắc hẳn chiếc xe sẽ hoạt động nếu pin không hoạt động. Biết được điều này, bạn sẽ tự hỏi pin ô tô dùng được bao lâu. Những nguồn năng lượng nhỏ gọn kỳ diệu này không tồn tại mãi mãi. Theo thời gian, khả năng giữ điện tích của chúng suy yếu và cuối cùng sẽ 'chết'. Mặc dù đây không phải là giải pháp lâu dài, nhưng có nhiều cách để kéo dài tuổi thọ của pin. Ví dụ như: sạc lại, hạn chế việc dừng lại và khởi động liên tục, tắt những tiện ích không cần thiết,...",
     quote:
-      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+      "Giống như con người, ô tô cũng cần 'dinh dưỡng' cho chuyến đi của mình",
   },
 
   {
@@ -38,9 +38,9 @@ const blogData = [
     time: "21h",
     imgUrl: img03,
     description:
-      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+      "Ngày nay, người lao động phải chịu rất nhiều áp lực vô hình từ gia đình, xã hội. Cuộc sống hẳn sẽ rất nhàm chán, quẩn quanh nếu không biết tự chăm sóc cho bản thân, tệ hơn sẽ dẫn tới trầm cảm và các bệnh về tim mạch. Vậy nên khi có cơ hội, hãy dành thời gian để vi vu trên chiếc xe của mình. Hãy lên kế hoạch, chuẩn bị đồ ăn thức uống, nghe bản nhạc yêu thích và chuẩn bị một tinh thần thoải mái để lên đường thôi! Hãy rủ gia đình, bạn bè đi cùng nữa nhé!",
     quote:
-      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+      "Cứ vui đi vì đời cho phép",
   },
 ];
 
--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from "react";
-import { Container, Row, Col, Form, FormGroup, Input } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 
 import { useParams } from "react-router-dom";
 import blogData from "../assets/data/blogData.js";
 import Helmet from "../components/Helmet/Helmet";
 import { Link } from "react-router-dom";
 
-import commentImg from "../assets/all-images/ava-1.jpg";
 
 import "../styles/blog-details.css";
 
@@ -44,58 +43,10 @@ const BlogDetails = () => {
 
                 <p className="section__description">{blog.description}</p>
                 <h6 className="ps-5 fw-normal">
-                  <blockquote className="fs-4">{blog.quote}</blockquote>
+                  <blockquote className="fs-4">" {blog.quote} "</blockquote>
                 </h6>
-                <p className="section__description">{blog.description}</p>
               </div>
 
-              <div className="comment__list mt-5">
-                <h4 className="mb-5">3 Comments</h4>
-
-                <div className="single__comment d-flex gap-3">
-                  <img src={commentImg} alt="" />
-                  <div className="comment__content">
-                    <h6 className=" fw-bold">David Visa</h6>
-                    <p className="section__description mb-0">14 July, 2022</p>
-                    <p className="section__description">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Eos nobis totam eius laborum molestias itaque minima
-                      distinctio, quae velit tempore!
-                    </p>
-
-                    <span className="replay d-flex align-items-center gap-1">
-                      <i class="ri-reply-line"></i> Replay
-                    </span>
-                  </div>
-                </div>
-
-                {/* =============== comment form ============ */}
-                <div className="leave__comment-form mt-5">
-                  <h4>Leave a Comment</h4>
-                  <p className="section__description">
-                    You must sign-in to make or comment a post
-                  </p>
-
-                  <Form>
-                    <FormGroup className=" d-flex gap-3">
-                      <Input type="text" placeholder="Full name" />
-                      <Input type="email" placeholder="Email" />
-                    </FormGroup>
-
-                    <FormGroup>
-                      <textarea
-                        rows="5"
-                        className="w-100 py-2 px-3"
-                        placeholder="Comment..."
-                      ></textarea>
-                    </FormGroup>
-
-                    <button className="btn comment__btn mt-3">
-                      Post a Comment
-                    </button>
-                  </Form>
-                </div>
-              </div>
             </Col>
 
             <Col lg="4" md="4">
