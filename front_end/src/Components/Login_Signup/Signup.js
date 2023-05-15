bb988dcd24d8c88831944ca1a323710bd64057ba
Tuan Nghia <tuannghia20062003@gmmail.com>
2023-05-15 14:17:31 +0700

    Translate

--- front_end/src/Components/Login_Signup/Signup.js
+++ front_end/src/Components/Login_Signup/Signup.js
@@ -45,32 +45,32 @@ function Signup() {
     return (
         <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
             <div className='bg-white p-3 rounded w-25'>
-                <h2>Sign Up</h2>
+                <h2>Đăng ký</h2>
                 <form action='' onSubmit={handleSubmit}>
                     <div className='mb-3'>
-                        <label htmlFor='name'><strong>Name </strong></label>
-                        <input type='text' placeholder='Enter your name' name='name'
+                        <label htmlFor='name'><strong>Tên </strong></label>
+                        <input type='text' placeholder='Nhập tên của bạn' name='name'
                             onChange={handleInput} className='form-control rounded-0' />
                         {errors.name && <span className='text-danger'>{errors.name}</span>}
                     </div>
 
                     <div className='mb-3'>
                         <label htmlFor='email'><strong>Email </strong></label>
-                        <input type='email' placeholder='Enter your email' name='email'
+                        <input type='email' placeholder='Nhập email của bạn' name='email'
                             onChange={handleInput} className='form-control rounded-0' />
                         {errors.email && <span className='text-danger'>{errors.email}</span>}
                     </div>
 
                     <div className="mb-3">
-                        <label htmlFor="password" ><strong>Password </strong></label>
-                        <input type="password" placeholder="Enter password" name='password'
+                        <label htmlFor="password" ><strong>Mật khẩu </strong></label>
+                        <input type="password" placeholder="Nhập mật khẩu" name='password'
                             onChange={handleInput} className='form-control rounded-0' />
                         {errors.password && <span className='text-danger'>{errors.password}</span>}
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="Fullname" ><strong>Họ & tên </strong></label>
-                        <input type="fullname" placeholder="Enter fullname" name='full_name'
+                        <input type="fullname" placeholder="Nhập họ tên" name='full_name'
                             onChange={handleInput} className='form-control rounded-0' />
                         {/* {errors.password && <span className='text-danger'>{errors.password}</span>} */}
                     </div>
@@ -91,7 +91,7 @@ function Signup() {
 
                     <div className="mb-3">
                         <label htmlFor="Address" ><strong>Địa chỉ</strong></label>
-                        <input type="Address" placeholder="Address" name='address'
+                        <input type="Address" placeholder="Địa chỉ" name='address'
                             onChange={handleInput} className='form-control rounded-0' />
                         {/* {errors.password && <span className='text-danger'>{errors.password}</span>} */}
                     </div>
@@ -100,13 +100,13 @@ function Signup() {
 
                     <label>
                         <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
-                        You are agree to our terms and policies
+                        Bạn đồng ý với các điều khoản và chính sách của chúng tôi
                     </label>
 
                     <button type="submit" className={`btn btn-success w-100 rounded-0 ${isChecked ? "" : "disabled"}`}  disabled={!isChecked} >
-                        Sign up
+                        Đăng ký
                     </button>
-                    <Link to='/login' className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Login</Link>
+                    <Link to='/login' className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Đăng nhập</Link>
                 </form>
             </div>
         </div>
